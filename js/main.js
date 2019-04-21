5fc09c82e33227893b42395177b3c48ac94adb3d
bit-01 <bassel_af@ymail.com>
2019-04-21 14:00:25 +0300

    navbar modifications

--- js/main.js
+++ js/main.js
@@ -9,16 +9,15 @@ $(function () {
             $navbar[0].classList.replace('eastBay-bg', 'snow-bg')
             $nav[0].classList.replace('snow-fg', 'eastBay-fg')
             $navbar.toggleClass('fixed-top', true)
-            // $brand.toggleClass('d-none', false)
             $brand[0].classList.replace('hide', 'show')
             $toggle.toggleClass('navbar-toggler-scrolled', true)
         } else {
             $navbar[0].classList.replace('snow-bg', 'eastBay-bg')
             $nav[0].classList.replace('eastBay-fg', 'snow-fg')
             $navbar.toggleClass('fixed-top', false)
-            // $brand.toggleClass('d-none', true)
             $brand[0].classList.replace('show', 'hide')
             $toggle.toggleClass('navbar-toggler-scrolled', false)
+            
         }
         
     });
