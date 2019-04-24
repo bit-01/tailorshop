3d77d54893b6bdbcea8dacbfda3e7852f7a65f10
bit-01 <bassel_af@ymail.com>
2019-04-24 03:18:17 +0300

    added alteration page

--- js/main.js
+++ js/main.js
@@ -9,20 +9,29 @@ $(function () {
         var $toggle = $('.navbar-toggler');
         if ($(this).scrollTop() > $main.height()) { 
           $items.toggleClass('small-fg', true)  
-          $navbar[0].classList.replace('eastBay-bg', 'snow-bg')
-          $nav[0].classList.replace('snow-fg', 'eastBay-fg')
           $navbar.toggleClass('fixed-top', true)
           $brand[0].classList.replace('hide', 'show')
           $toggle.toggleClass('navbar-toggler-scrolled', true)
+
+          if ($('.main').length > 0) {
+            $navbar[0].classList.replace('eastBay-bg', 'snow-bg')
+            $nav[0].classList.replace('snow-fg', 'eastBay-fg')
+          } else {
+            $('.page-title')[0].style.bottom = '70px'
+          }
             
         } else {
           $items.toggleClass('small-fg', false)
-          $navbar[0].classList.replace('snow-bg', 'eastBay-bg')
-          $nav[0].classList.replace('eastBay-fg', 'snow-fg')
           $navbar.toggleClass('fixed-top', false)
           $brand[0].classList.replace('show', 'hide')
           $toggle.toggleClass('navbar-toggler-scrolled', false)
-            
+           
+          if ($('.main').length > 0) {
+            $navbar[0].classList.replace('snow-bg', 'eastBay-bg')
+            $nav[0].classList.replace('eastBay-fg', 'snow-fg')
+          } else {
+            $('.page-title')[0].style.bottom = '0px'
+          }
         }
         
     });
